a3fa0b8ed788c5ac20babbb9affc7b56b9afb60a
yuji yamada <ymdarudy@gmail.com>
2022-06-11 18:44:18 +0900

    add signin function & localizing into Japanese

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :pictures
+
+  has_secure_password
+  before_validation { email.downcase! }
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "pictures#index"
+  resources :users, only: [:new, :create, :show]
   resources :pictures
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
